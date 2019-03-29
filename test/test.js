76bfc53ebf793de0c57d4c2def349162e1b9b258
Saurabh M. Machave <unknown7bolt@gmail.com>
2019-03-29 07:52:54 +0530

    increase before all timeout to 5000

--- test/test.js
+++ test/test.js
@@ -34,6 +34,7 @@ const initialValues = {
 };
 
 before(async () => {
+  this.timeout(5000);
   await createAdminUser();
   connection = nano(`http://${USERNAME}:${PASSWORD}@${HOSTNAME}:${PORT}`);
   await connection.db.create(DATABASE_NAME);
